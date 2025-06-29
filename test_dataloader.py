7829a4e173b2bd679162b464541ea55b503d3186
bhagavath12 <123378341+bhagavath12@users.noreply.github.com>
2025-06-29 22:13:58 +0530

    Update test_dataloader.py

--- test_dataloader.py
+++ test_dataloader.py
@@ -3,6 +3,6 @@ from src.dataloader import create_dataset
 dataset, tokenizer = create_dataset()
 
 for img_tensor, caption in dataset.take(1):
-    print("🖼️ Image feature shape:", img_tensor.shape)
-    print("📝 Caption shape:", caption.shape)
+    print(" Image feature shape:", img_tensor.shape)
+    print(" Caption shape:", caption.shape)
     break
