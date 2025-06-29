56ce6a03d5db43fcd33881d715d608e25808746f
bhagavath12 <123378341+bhagavath12@users.noreply.github.com>
2025-06-29 22:02:10 +0530

    Update config.py

---
+++ src/config.py
@@ -0,0 +1,17 @@
+# src/config.py
+
+IMAGE_SIZE = (299, 299)
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+
+EMBEDDING_DIM = 256
+UNITS = 512
+EPOCHS = 20
+
+VOCAB_SIZE = 5000
+MAX_CAPTION_LEN = 40
+FEATURES_SHAPE = 2048
+
+CAPTIONS_FILE = "data/captions.txt"
+IMAGES_DIR = "data/images/"
+FEATURES_DIR = "data/features/"
