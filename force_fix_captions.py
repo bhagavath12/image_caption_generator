d3ef13149fc2650a6a6bc844b69045f7ab9c5b39
bhagavath12 <123378341+bhagavath12@users.noreply.github.com>
2025-06-29 22:13:10 +0530

    Update force_fix_captions.py

--- force_fix_captions.py
+++ force_fix_captions.py
@@ -7,4 +7,4 @@ df = pd.read_csv(path)
 df['caption'] = df['caption'].apply(lambda x: f"<start> {x.strip().replace('<start>', '').replace('<end>', '')} <end>")
 
 df.to_csv(path, index=False)
-print("✅ Captions file forcibly cleaned with proper <start> and <end>")
+print(" Captions file forcibly cleaned with proper <start> and <end>")
